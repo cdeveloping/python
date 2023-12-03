f365ec2622457d58b4616428107b9d5ba2b27bd9
cdeveloping <carlosdeveloper@outlook.es>
2023-12-03 08:30:46 +0100

    Tipos  de datos básicos
    Tipos de datos básicos en Python

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Esto es un comentario: 
+
+# Nuestro Hola Mundo en Python
+print ("Hola Python")
+
+"""
+Este es un comentario
+en varias Lineas
+"""
+
+#Consultar el tipo de dato
+
+print(type("Soy un dato str o string")) # Es una cadena de texto o string
+print(type(5)) # Es un dato tipo int
+print(type(1.5)) # Esto es un dato de tipo float
+print(type(True)) # Esto es un tipo de dato boolean
+print(type(3 + 1j)) # Esto es un tipo de dato complejo
